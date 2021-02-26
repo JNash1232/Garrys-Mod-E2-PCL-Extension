d6445b499ddbef35b649bca8de00d1ad2f52371f
Kieran <github@andersons.bz>
2021-02-26 06:15:36 +0000

    Removed FPP requirement

--- lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
+++ lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
@@ -10,46 +10,46 @@ __e2setcost(50)
 
 --- propfreeze, 1 = frozen, 0 = unfrozen, pretty much same as propcore
 e2function void entity:propFreeze(number frozen)
-	if not IsValid(this) or this:IsPlayer() 
-		or not this:CPPICanPhysgun(self.player) then
-		return nil
+    if not IsValid(this) or this:IsPlayer() 
+        or (this.CPPICanPhysgun and not this:CPPICanPhysgun(self.player)) then
+        return nil
     end
 
-	local phys = this:GetPhysicsObject()
-	phys:EnableMotion(frozen <= 0)
-	phys:Wake()
+    local phys = this:GetPhysicsObject()
+    phys:EnableMotion(frozen <= 0)
+    phys:Wake()
 end
 
 --- sets gravity
 e2function void entity:enableGravity(number active)
-	if not IsValid(this) or this:IsPlayer() 
-		or not this:CPPICanTool(self.player, "toolgun") then
-		return nil
-	end
+    if not IsValid(this) or this:IsPlayer() 
+        or (this.CPPICanPhysgun and not this:CPPICanTool(self.player, "toolgun")) then
+        return nil
+    end
 
-	local phys = this:GetPhysicsObject()
-	phys:EnableGravity(active >= 1)
-	phys:Wake()
+    local phys = this:GetPhysicsObject()
+    phys:EnableGravity(active >= 1)
+    phys:Wake()
 end
 
 --- enableDrag ... it enables/disbales drag
 e2function void entity:enableDrag(number active)
     if not IsValid(this) or this:IsPlayer() 
-		or not this:CPPICanTool(self.player, "toolgun") then
-		return nil
-	end
+        or (this.CPPICanPhysgun and not this:CPPICanTool(self.player, "toolgun")) then
+        return nil
+    end
 
-	local phys = this:GetPhysicsObject()
-	phys:EnableDrag(active >= 1)
-	phys:Wake()
+    local phys = this:GetPhysicsObject()
+    phys:EnableDrag(active >= 1)
+    phys:Wake()
 end
 
 --- propsolid sets the solidity of the prop 
 e2function void entity:propNotSolid(number solid)
-	if not IsValid(this) or this:IsPlayer() 
-		or not this:CPPICanTool(self.player, "toolgun") then
-		return nil
-	end
+    if not IsValid(this) or this:IsPlayer() 
+        or (this.CPPICanPhysgun and not this:CPPICanTool(self.player, "toolgun")) then
+        return nil
+    end
 
     this:SetSolid(solid >= 1 and 0 or 6)
 end
@@ -57,9 +57,9 @@ end
 --- drawShadow sets if E should draw shadows
 e2function void entity:drawShadow(number draw)
     if not IsValid(this) or this:IsPlayer() 
-		or not this:CPPICanTool(self.player, "toolgun") then
-		return nil
-	end
+        or (this.CPPICanPhysgun and not this:CPPICanTool(self.player, "toolgun")) then
+        return nil
+    end
 
     this:DrawShadow(draw >= 1)
 end
